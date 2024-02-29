aef9572976ad7b16399ffea01ffaed23a7fd2304
Kayke-Ti <kaykeloiola@gmail.com>
2024-02-29 13:11:38 -0300

    feat: config new in pages

--- web/src/Pages/Monitors.jsx
+++ web/src/Pages/Monitors.jsx
@@ -1,5 +1,14 @@
+import LoanChart from "../components/LoanChart";
+
 const Monitors = () => {
-  return <h1>hello Monitors</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-96 bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">Gráfico de Empréstimos</h1>
+        <LoanChart />
+      </div>
+    </div>
+  );
 };
 
 export default Monitors;
