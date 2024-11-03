269fce5c4d6fbe491b56309eb807e733f15075fe
kaykeeb3 <kaykeloioladev@gmail.com>
2024-11-03 12:52:59 -0300

    feat: config server (run server)

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,12 +1,39 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+import http from "http";
+
+import { Socket } from "@/infra/socket/socket";
+import authRoutes from "@/infra/routes/authRoutes";
+import bookRoutes from "@/infra/routes/bookRoutes";
+import loanRoutes from "@/infra/routes/loanRoutes";
+import equipmentRoutes from "@/infra/routes/equipmentRoutes";
+import scheduleRoutes from "@/infra/routes/scheduleRoutes";
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Servidor está rodando!");
+// Configuração do CORS
+const corsOptions = {
+  origin: "*",
+  methods: "*",
+};
+
+// Uso de middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/books", bookRoutes);
+app.use("/loans", loanRoutes);
+app.use("/equipments", equipmentRoutes);
+app.use("/schedules", scheduleRoutes);
+
+// Configuração do Socket.io
+const { io, cleanup } = Socket(server);
+
+// Iniciar o servidor
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+export { server, io, cleanup };
