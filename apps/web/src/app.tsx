b1cf96e01fbd17756c029df432f02497a1f6a100
kaykeeb3 <kaykeloioladev@gmail.com>
2024-11-03 11:23:32 -0300

    chore: creating web project adding web configuration (installing dependencies, configuring shadcn/ui and .eslintrc)

---
+++ apps/web/src/app.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <Button className="mt-20">button</Button>
+    </div>
+  );
+}
