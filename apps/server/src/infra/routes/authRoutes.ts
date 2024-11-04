1168b072aa03617a7be88ad889a74d320529a82b
kaykeeb3 <kaykeloioladev@gmail.com>
2024-11-04 12:37:07 -0300

    feat: add new user token verification route (get)

--- apps/server/src/infra/routes/authRoutes.ts
+++ apps/server/src/infra/routes/authRoutes.ts
@@ -7,6 +7,7 @@ const authController = new AuthController();
 router.post("/register", authController.register.bind(authController));
 router.post("/login", authController.login.bind(authController));
 router.get("/profile", authController.verifyAccess.bind(authController));
+router.get("/verify", authController.verifyAccess.bind(authController));
 router.get("/users", authController.getAllUsers.bind(authController));
 router.get("/users/:id", authController.getUserById.bind(authController));
 router.put("/users/:id", authController.updateUser.bind(authController));
