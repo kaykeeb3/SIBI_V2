dd1273182706a9ae09e5be9bcd90ac6f00205c1a
kaykeeb3 <kaykeloioladev@gmail.com>
2024-11-16 14:18:50 -0300

    feat: create component (whatsapp link) ctt & config pages

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "@/components/header";
 import { Separator } from "@/components/ui/separator";
 import { CustomToaster } from "@/components/custom-toaster";
+import { WhatsappButton } from "./components/whatsapp-button";
 
 export function App() {
   const location = useLocation();
@@ -13,9 +14,10 @@ export function App() {
     <div className="flex flex-col w-full min-h-screen">
       {!isLoginPage && <Header />}
       {!isLoginPage && <Separator className="bg-zinc-300 w-full" />}
+      {!isLoginPage && <WhatsappButton />}
       <div className="flex">
         {!isLoginPage && <NavBar />}
-        <div className="flex-1 flex items-center justify-center text-slate-50">
+        <div className="flex-1 flex items-center justify-center text-red-500">
           <Outlet />
         </div>
         <CustomToaster />
