52d5c9f6473c9f26e52506109184f368ac81a228
kaykeeb3 <kaykeloioladev@gmail.com>
2024-11-21 15:19:00 -0300

    fix: remove scheme in book

---
+++ apps/server/prisma/migrations/20241121181754_remove_created_at_to_book/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Book` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Book" DROP COLUMN "createdAt";
