a1c3cf00845bebe59d1d236cf4df7664319a1619
kaykeeb3 <kaykeloioladev@gmail.com>
2024-12-05 12:21:38 -0300

    feat: add pages initial setup & config;

---
+++ apps/app/src/pages/app/book/book.tsx
@@ -0,0 +1,11 @@
+export function Book() {
+  return (
+    <div className="col p-5 overflow-auto h-100">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="mb-4 mt-0">Livros</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ apps/app/src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+export function Dashboard() {
+  return (
+    <div className="col p-5 overflow-auto h-100">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="mb-4 mt-0">Dashboard</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
