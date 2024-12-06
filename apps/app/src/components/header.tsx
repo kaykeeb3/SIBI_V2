a8eee1d949fd81b7c992d1972283318db4ef3ab9
kaykeeb3 <kaykeloioladev@gmail.com>
2024-12-06 13:19:23 -0300

    feat: add new edit profile (modal & styles);

--- apps/app/src/components/header.tsx
+++ apps/app/src/components/header.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { getProfile, updateProfile } from "../services/auth/auth-service";
-import { Drawer, Button, Form, Input, Avatar } from "rsuite";
+import { Modal, Button, Form, Input, Avatar } from "rsuite";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import socketService, { Notification } from "../services/socket/socket-service";
@@ -16,18 +16,37 @@ interface Profile {
   phone: string;
 }
 
+const initialFormData = {
+  name: "",
+  profilePicture: "",
+  institution: "",
+};
+
 export function Header() {
   const [profile, setProfile] = useState<Profile | null>(null);
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    profilePicture: "",
-    institution: "",
-  });
+  const [openModal, setOpenModal] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
   const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const getFirstName = useCallback(
+    (name: string | undefined) => name?.split(" ")[0] || "Usuário",
+    []
+  );
+
+  const getInstitutionName = useCallback((institution: string | undefined) => {
+    if (!institution) return "Instituição desconhecida";
+    const words = institution.split(" ");
+    return words[0] === "EEEP" || words[0] === "Escola"
+      ? words.slice(1, 3).join(" ")
+      : words.slice(0, 2).join(" ");
+  }, []);
 
   const fetchProfile = useCallback(async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    setIsLoading(true);
     try {
       const data = await getProfile();
       setProfile(data);
@@ -37,123 +56,91 @@ export function Header() {
         institution: data.institution,
       });
     } catch {
-      if (!handleLogout) {
-        toast.error("Erro ao buscar perfil.");
-      }
+      toast.error("Erro ao buscar perfil.");
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
-  const handleNotifications = (newNotifications: Notification[]) => {
-    setNotifications((prevNotifications) => [
-      ...prevNotifications,
-      ...newNotifications,
-    ]);
+  const handleNotifications = useCallback((newNotifications: Notification[]) => {
+    setNotifications((prev) => [...prev, ...newNotifications]);
     toast.info(`Você tem ${newNotifications.length} novas notificações!`);
-  };
+  }, []);
 
 
   useEffect(() => {
-    fetchProfile();
-
-
-    socketService.connect();
-    socketService.onOverdueNotifications(handleNotifications);
-
-
-    return () => {
-      socketService.disconnect();
-    };
-  }, [fetchProfile]);
-
-  const getFirstName = (name: string | undefined) => {
-    return name?.split(" ")[0] || "Usuário";
-  };
-
-  const getInstitutionName = (institution: string | undefined) => {
-    if (!institution) return "Instituição desconhecida";
-
-    const words = institution.split(" ");
-
-    if (words[0] === "EEEP" || words[0] === "Escola") {
-      return words.slice(1, 3).join(" ");
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetchProfile();
+      socketService.connect();
+      socketService.onOverdueNotifications(handleNotifications);
     }
 
-    return words.slice(0, 2).join(" ");
-  };
+    return () => socketService.disconnect();
+  }, [fetchProfile, handleNotifications]);
 
-
-
-  const handleInputChange = (value: string, name: string) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
 
   const mutation = useMutation({
-    mutationFn: (formData: { name: string; profilePicture: string; institution: string }) =>
-      updateProfile(profile?.id || "", formData),
+    mutationFn: (data: typeof initialFormData) =>
+      updateProfile(profile?.id || "", data),
     onSuccess: () => {
-      setProfile({
-        ...profile!,
-        ...formData,
-      });
-      setOpenDrawer(false);
+      setProfile((prev) => ({ ...prev!, ...formData }));
+      setOpenModal(false);
       toast.success("Perfil atualizado com sucesso!");
     },
-    onError: () => {
-      toast.error("Erro ao atualizar perfil!");
-    },
+    onError: () => toast.error("Erro ao atualizar perfil!"),
   });
 
-  const handleSave = () => {
-    mutation.mutate(formData);
-  };
+  const handleInputChange = useCallback(
+    (value: string, name: string) =>
+      setFormData((prevData) => ({ ...prevData, [name]: value })),
+    []
+  );
 
-  const handleLogout = () => {
+  const handleSave = () => mutation.mutate(formData);
+
+  const handleLogout = useCallback(() => {
     localStorage.removeItem("token");
     window.location.href = "/";
-  };
+  }, []);
+
+  const renderProfileInfo = useMemo(() => {
+    if (isLoading) return <span className="text-white">Carregando...</span>;
+
+    if (profile) {
+      return (
+        <>
+          <div className="px-2">
+            <span className="d-block m-0 p-0 text-white">
+              Olá, {getFirstName(profile.name)}
+            </span>
+            <small className="m-0 p-0">
+              {getInstitutionName(profile.institution)}
+            </small>
+          </div>
+          <Avatar src={profile.profilePicture || undefined} circle />
+          {notifications.length > 0 && (
+            <Button className="px-2 py-0 bg-danger text-white shadow-none border-0">
+              {notifications.length > 99 ? "99+" : notifications.length} Novas
+            </Button>
+          )}
+        </>
+      );
+    }
+    return null;
+  }, [isLoading, profile, notifications, getFirstName, getInstitutionName]);
 
   return (
     <>
       <header className="container-fluid d-flex justify-content-end">
-        <div className="d-flex align-items-center">
-          {profile ? (
-            <>
-              <div className="px-2">
-                <span className="d-block m-0 p-0 text-white">
-                  Olá, {getFirstName(profile.name)}
-                </span>
-                <small className="m-0 p-0">
-                  {getInstitutionName(profile.institution)}
-                </small>
-              </div>
+        <div className="d-flex align-items-center">{renderProfileInfo}</div>
 
-              <Avatar
-                src={profile.profilePicture || undefined}
-                circle
-              />
-
-              {notifications.length > 0 && (
-                <Button className="px-2 py-0 bg-danger text-white shadow-none border-0">
-                  {notifications.length > 99 ? "99+" : notifications.length} Novas
-                </Button>
-              )}
-            </>
-          ) : (
-            <div>
-              <span className="text-white">Carregando...</span>
-            </div>
-          )}
-
-          <Button
-            className="px-0 py-0 bg-transparent shadow-none border-0"
-            onClick={() => setOpenDrawer(true)}
-          >
-            <span className="mdi mdi-chevron-down text-white status-line"></span>
-          </Button>
-        </div>
+        <Button
+          onClick={() => setOpenModal(true)}
+          className="px-0 py-0 bg-transparent shadow-none border-0"
+        >
+          <span className="mdi mdi-chevron-down text-white status-line"></span>
+        </Button>
 
         <Button
           onClick={handleLogout}
@@ -163,86 +150,78 @@ export function Header() {
         </Button>
       </header>
 
-      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <Drawer.Header>
-          <Drawer.Title>Editar Perfil</Drawer.Title>
-        </Drawer.Header>
+      {/* Modal */}
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        size="sm"
+        backdrop="static"
+      >
+        <Modal.Header>
+          <Modal.Title>Editar Perfil</Modal.Title>
+        </Modal.Header>
 
-        <Drawer.Body>
-          <Form fluid>
-            <div className="row mb-3">
-              {/* Nome */}
-              <div className="col-12 col-md-6">
-                <Form.Group>
-                  <Form.ControlLabel>Nome</Form.ControlLabel>
-                  <Input
-                    name="name"
-                    value={formData.name}
-                    onChange={(value) => handleInputChange(value, "name")}
-                    placeholder="Digite o nome"
-                  />
-                </Form.Group>
-              </div>
-
-              {/* Instituição */}
-              <div className="col-12 col-md-6">
-                <Form.Group>
-                  <Form.ControlLabel>Instituição</Form.ControlLabel>
-                  <Input
-                    name="institution"
-                    value={formData.institution}
-                    onChange={(value) => handleInputChange(value, "institution")}
-                    placeholder="Digite o nome da instituição"
-                  />
-                </Form.Group>
-              </div>
-            </div>
-
-            {/* Foto de Perfil (URL) */}
-            <div className="row mb-3">
-              <div className="col-12">
-                <Form.Group>
-                  <Form.ControlLabel>Foto de Perfil (URL)</Form.ControlLabel>
-                  <Input
-                    name="profilePicture"
-                    value={formData.profilePicture}
-                    onChange={(value) => handleInputChange(value, "profilePicture")}
-                    placeholder="Digite a URL da foto"
-                  />
-                </Form.Group>
-              </div>
-            </div>
+        <Modal.Body>
+          <div className="d-flex justify-content-center">
+            <Avatar size="xxl" src={formData.profilePicture || undefined} circle />
+          </div>
+          <Form fluid className="px-1">
+            <Form.Group>
+              <Form.ControlLabel>
+                Nome<span className="text-danger">*</span>
+              </Form.ControlLabel>
+              <Input
+                name="name"
+                value={formData.name}
+                onChange={(value) => handleInputChange(value, "name")}
+                placeholder="Digite o nome"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.ControlLabel>
+                Instituição<span className="text-danger">*</span>
+              </Form.ControlLabel>
+              <Input
+                name="institution"
+                value={formData.institution}
+                onChange={(value) => handleInputChange(value, "institution")}
+                placeholder="Digite o nome da instituição"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.ControlLabel>
+                Foto de Perfil (URL)<span className="text-danger">*</span>
+              </Form.ControlLabel>
+              <Input
+                name="profilePicture"
+                value={formData.profilePicture}
+                onChange={(value) => handleInputChange(value, "profilePicture")}
+                placeholder="Digite a URL da foto"
+              />
+            </Form.Group>
           </Form>
+        </Modal.Body>
 
-          <Drawer.Actions>
-            <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-              <Button
-                onClick={handleSave}
-                block
-                disabled={mutation.isPending}
-                color="blue"
-                appearance="primary"
-                className="btn-lg"
-                loading={mutation.isPending}
-              >
-                {mutation.isPending ? "Carregando..." : "Confirmar"}
-              </Button>
-
-              <Button
-                onClick={() => setOpenDrawer(false)}
-                block
-                color="red"
-                appearance="primary"
-                className="btn-lg"
-              >
-                Cancelar
-              </Button>
-
-
-            </div>
-          </Drawer.Actions>
-        </Drawer.Body>
-      </Drawer>
+        <Modal.Footer>
+          <Button
+            onClick={handleSave}
+            loading={mutation.isPending}
+            color="blue"
+            appearance="primary"
+            block
+          >
+            {mutation.isPending ? "Carregando..." : "Confirmar"}
+          </Button>
+          <Button
+            onClick={() => setOpenModal(false)}
+            color="red"
+            appearance="primary"
+            block
+          >
+            Cancelar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
